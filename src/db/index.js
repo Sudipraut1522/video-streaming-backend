809c4d1c8a0d14340c1a3b595818321cf844cc1f
sudip <sudipraut444@dmail.com>
2024-03-02 20:19:09 +0545

    connection to mongoos

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants.js"
+import dotenv from "dotenv"
+dotenv.config()
+
+export const connectionDB = async () => {
+  try {
+    console.log("connection", process.env.MONGOOSE_URL)
+    const connection_DB = mongoose.connect(process.env.MONGOOSE_URL)
+    // .then(() => console.log("database connected"))
+    // .catch((err) => console.log(err))
+    console.log("connected", process.env.MONGOOSE_URL)
+  } catch (error) {
+    console.log("errors", error)
+    process.exit(1)
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import "dotenv/config"
+
+import { connectionDB } from "./db/index.js"
+import mongoose from "mongoose"
+
+// dotenv.config({
+//   path: "./env",
+// })
+
+connectionDB()
